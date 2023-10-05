59d621a81f674b640e3d64f7f5a38d9fc809df0b
Digui87 <d_salvador110@outlook.com>
2023-10-05 13:41:48 -0300

    estilização

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,44 @@
 <script setup>
+import { reactive } from 'vue';
+
+const aritmetica = reactive({
+  somar: 'somar',
+  diminuir: 'diminuir',
+  multiplicar: 'multiplicar',
+  dividir: 'dividir'
+})
+
+
 </script>
 
 <template>
+  <div class="container">
+    <header class="p-5 mb-5 mt-5 bg-secondary rounded-3">
+      <h1>Calculdadora Aritmética</h1>
+    </header>
+    <div class="row">
+      <div class="col-md-5">
+        <input type="number" placeholder="Número">
+      </div>
+      <div class="col-md-2">
+        <select class="form-control">
+          <option value="somar">Somar</option>
+          <option value="diminuir">Diminuir</option>
+          <option value="multiplicar">Multiplicar</option>
+          <option value="dividir">Dividir</option>
+        </select>
+      </div>
+      <div class="col-md-5">
+        <input type="number" placeholder="Número">
+      </div>
+    </div>
+    <h2 class="mt-5">Resultado</h2>
+  </div>
 </template>
 
 <style scoped>
+.container {
+  max-width: 640px;
+  text-align: center;
+}
 </style>
