b1fabd9a1f1c179e7195e5ab7ca5c5d4f36b8a7b
Digui87 <d_salvador110@outlook.com>
2023-10-11 16:23:49 -0300

    ajustes

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,26 @@
 <script setup>
-import Calculadora from './components/Calculadora.vue';
+
+import { ref, computed } from 'vue';
+
+const numero1 = ref(0);
+const numero2 = ref(0);
+const operacao = ref('');
+
+const resultado = computed(() => {
+    switch (operacao.value) {
+        case 'somar':
+            return numero1.value + numero2.value;
+        case 'diminuir':
+            return numero1.value - numero2.value;
+        case 'multiplicar':
+            return numero1.value * numero2.value;
+        case 'dividir':
+            return numero1.value / numero2.value;
+        default:
+            return 0;
+    }
+})
+
 </script>
 
 <template>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Calculadora from './components/Calculadora.vue';
 
 createApp(App).mount('#app')
